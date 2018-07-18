397198357039783bb5700fec7643ed95ea8e0cab
Kyle-Neale <kyle.a.neale@gmail.com>
2018-07-18 18:55:11 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    sentences = self.split(' ')
+    sentences.each do |sentence|
+      if sentence.end_with?("!") || sentence.end_with?("?") || sentence.end_with?(".")
+        count += 1
+      end
+    end
+    count
   end
+
 end
